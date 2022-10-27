5b739f29309ace47d24894ef95dd9a55d36f7a3e
Isaiah Henson <Devpuddles@gmail.com>
2022-10-27 17:50:56 -0400

    building functions

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-function fetchIssues () {
+document.getElementById('issueInputForm').addEventListener('submit',
+    saveIssue)
+
+function fetchIssues() {
     let issues = JSON.parse(localStorage.getItem('issues'))
     let issuesList = document.getElementById('issuesList')
 
     issuesList.innerHTML = '';
 
-    for(let i=0; i < issues.length; i++) {
+    for (let i = 0; i < issues.length; i++) {
         let id = issues[i].id
         let subject = issues[i].subject
         let description = issues[i].description
         let serverity = issues[i].serverity
         let assignedTo = issues[i].assignedTo
         let status = issues[i].status
+        let statusColor = status == "Closed" ? 'label-success' :
+            'label-info'
+
+        //issuesList.innerHTML += 
+    }
+}
+
+function saveIssue(e) {
+    let issueId = chance.guid()
+    let issueSubject = document.getElementById('issueSubjInput').value
+    let issueDesc = document.getElementById('issueSubjInput').value
+    let issueSeverity = document.getElementById('issueSubjInput').value
+    let issueAssignedTo = document.getElementById('issueSubjInput').value
+    let issueStatus = 'Open'
+
+    let issue = {
+        id: issueId,
+        subject: issueSubject,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
     }
 }
